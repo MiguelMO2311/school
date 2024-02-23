863c4572637895a05052d9cfe6bd44b689265363
MiguelMO2311 <mmeneses73@gmail.com>
2024-02-23 21:57:38 +0100

    uniones de tablas y herramientas

--- dataBaseConnect3.js
+++ dataBaseConnect3.js
@@ -57,7 +57,7 @@ async function main() {
         let [result1] = await connection.query(sql1, params1);
         console.log('Datos obtenidos para el Reto 1:');
         console.log(result1);
-        
+
 
         // Reto 2. • Obtén todos los nombres y apellidos de los profesores y los nombres de las asignaturas que imparten.
         let params2 = [];
@@ -69,7 +69,7 @@ async function main() {
         let [result2] = await connection.query(sql2, params2);
         console.log('Datos obtenidos para el Reto 2:');
         console.log(result2);
-        
+
         // Reto 3. • Obtén el número total de alumnos por asignatura, el nombre de la asignatura y el nombre y apellidos del profesor que la imparte.
         let params3 = [];
         let sql3 = `SELECT COUNT(students.student_id) AS total_students, subjects.title, teachers.first_name, teachers.last_name 
@@ -83,8 +83,32 @@ async function main() {
         let [result3] = await connection.query(sql3, params3);
         console.log('Datos obtenidos para el Reto 3:');
         console.log(result3);
-        
-        // Cierra la conexión.
+           // Reto 1  *** OPCIONAL ***• Obtén el nombre y apellido de todos los profesores, la asignatura que imparten y la nota media de la asignatura que imparten.
+           let params4 = [];
+           let sql4 = `SELECT teachers.first_name, teachers.last_name, subjects.title, AVG(marks.mark) AS AVG_MARK
+           FROM teachers
+           LEFT JOIN subjects ON teachers.teacher_id = subjects.subject_id
+           LEFT JOIN marks ON subjects.subject_id = marks.subject_id
+           GROUP BY teachers.teacher_id, marks.mark_id;
+           `;
+           let [result4] = await connection.query(sql4, params4);
+           console.log('Datos obtenidos para el Reto 1 *** OPCIONAL ****:');
+           console.log(result4);
+   
+   
+           //Reto 2 *** OPCIONAL ***• Al finalizar el curso, queremos hacer un reporte completo de los alumnos del bootcamp para poder mejorar en el futuro.
+           //  Para ello se pide listar el nombre y apellidos del alumno, el número de asignaturas que ha cursado, la nota media de todas ellas,
+           //  la nota más alta y la nota más baja conseguida.
+           let params5 = [];
+           let sql5 = `SELECT students.first_name, students.last_name, COUNT(marks.mark_id), AVG(marks.mark), MAX(marks.mark), MIN(marks.mark)
+           FROM students
+           JOIN marks ON students.student_id = marks.student_id
+           JOIN subjects ON marks.subject_id = subjects.subject_id
+           GROUP BY students.student_id;`;
+           let [result5] = await connection.query(sql5, params5);
+           console.log('Datos obtenidos para el Reto 2*** OPCIONAL ****:');
+           console.log(result5);
+   
         await connection.end();
     } catch (error) {
         console.error('Ha ocurrido un error:', error);
